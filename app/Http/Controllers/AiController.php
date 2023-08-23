8896b244c0e71b5de009dd311b7d704cd70bbf6e
Aszódi Petra <h042690@stud.u-szeged.hu>
2023-08-23 20:57:47 +0200

    frontend

--- app/Http/Controllers/AiController.php
+++ app/Http/Controllers/AiController.php
@@ -13,18 +13,25 @@ class AiController extends Controller
     }
 
     public function makeRequest(Request $request) {
-        $yourApiKey = "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";
-        $client = OpenAI::client($yourApiKey);
+        $apiKey = env('API_KEY');
+        $client = OpenAI::client($apiKey);
+
+        $user_message = "I'm $request->age years old $request->gender, and I weight around $request->weight kg. I want to work out $request->place $request->frequency, focusing on $request->focus. My goal is $request->goal.";
+        
+        if($request->extra != null){
+            $user_message = "$user_message And the following should also be taken into account: $request->extra";
+        }
 
         $result = $client->chat()->create([
             'model' => 'gpt-3.5-turbo-0613',
             'messages' => [
-                ['role' => 'user', 'content' => $request->prompt]
+                ['role' => 'user', 'content' => $user_message]
             ],
         ]);
 
         $messageContent = $result['choices'][0]['message']['content'];
 
-        dd($messageContent); // an open-source, widely-used, server-side scripting language.
+        dd($messageContent);
+        
     }
 }
--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -1,17 +1,48 @@
 <template>
-    <h1>Hello</h1>
-
+    <div>
+        <h1>Generate your own workout plan</h1>
+    </div>
+    
+    <div class="form-group">
     <form @submit.prevent="onFormSubmit">
-        <input type="text" v-model="form.prompt" />
-        <button type="submit">Send request</button>
+        <label>Age</label>
+        <input type="number" v-model="form.age" />
+        <label>Gender</label>
+        <input type="text" v-model="form.gender" />
+        <label>Weight (kg)</label>
+        <input type="number" v-model="form.weight" />
+        <label>Where do you want to workout?</label>
+        <input type="text" placeholder="for example at home, in the gym" v-model="form.place" />
+        <label>How frequently do you want to workout?</label>
+        <input type="text" placeholder="for example three times a week" v-model="form.frequency" />
+        <label>What do you want to focus on?</label>
+        <input type="text" placeholder="for example abs, upper body" v-model="form.focus" />
+        <label>What is your goal with working out?</label>
+        <input type="text" placeholder="for example gaining musle, losing weight" v-model="form.goal" />
+        <label>Any additional information</label>
+        <input type="text" placeholder="for example prefer pilates, hate squats" v-model="form.extra" />
+        <button type="submit">Make me a plan!</button>
     </form>
+    </div>
+        <p class="plan"> Your workout plan goes here: </p>
+    <div>
+       <!-- TODO -->
+    </div>
 </template>
 
 <script setup>
     import { useForm } from '@inertiajs/vue3'
+
     let form = useForm({
-        prompt: ''
-    })
+        age: '',
+        gender: '',
+        weight: '',
+        place: '',
+        frequency: '',
+        focus: '',
+        goal: '',
+        extra: '',
+    });
 
     const onFormSubmit = () => {
         form.post('/ai');
